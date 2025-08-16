31adc5b522d2f8c4b7d5dfe12a77c74ce96a9fd9
Alex1ne <imalex1ne@gmail.com>
2025-08-15 19:49:27 -0600

    project upload

---
+++ Scenes/Player/Player.cs
@@ -0,0 +1,71 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody
+{
+	private const float _PlayerSpeed = 8.0f;
+	private const float _PlayerJumpVelocity = 12.0f;
+	private const float _Gravity = -24.0f;
+	private const double _MouseSensitivity = 0.002;
+	
+	Vector3 velocity =  Vector3.Zero; // we have to make a velocity vector3 for ourselves and use it at the end of the physics process. the character here does not have a velocity property already defined.
+	
+	[Export] private NodePath _PlayerCameraPath;
+	[Export] private NodePath _PlayerCameraRayCastPath;
+	
+	private Camera _PlayerCamera;
+	private RayCast _PlayerCameraRayCast;
+	
+	public override void _Ready()
+	{
+		Input.SetMouseMode(Input.MouseModeEnum.Captured);
+		
+		_PlayerCamera = GetNode<Camera>(_PlayerCameraPath);
+		_PlayerCameraRayCast = GetNode<RayCast>(_PlayerCameraRayCastPath);
+	}
+	
+	public override void _UnhandledInput(InputEvent @event){
+		
+		if(@event is InputEventMouseMotion mouse_motion){
+			Vector3 rotation = Rotation;
+			Vector3 camera_rotation = _PlayerCamera.Rotation;
+			
+			rotation.y = rotation.y - mouse_motion.Relative.x * (float)_MouseSensitivity;
+			camera_rotation.x = camera_rotation.x - mouse_motion.Relative.y * (float)_MouseSensitivity;
+			
+			Rotation = rotation;
+			_PlayerCamera.Rotation = camera_rotation;
+			
+		}
+	}
+	
+	public override void _PhysicsProcess(float delta){
+		
+		if (!IsOnFloor()){
+			velocity += new Vector3(0, _Gravity, 0) * (float)delta;
+		}
+		
+		
+		if (Input.IsActionPressed("jump") && IsOnFloor()){
+			velocity.y = _PlayerJumpVelocity;
+		}
+		
+		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
+		
+		Vector3 direction = (Transform.basis * new Vector3(inputDir.x, 0, inputDir.y)).Normalized();
+		
+		if(direction!= Vector3.Zero){
+			velocity.x = direction.x * _PlayerSpeed;
+			velocity.z = direction.z * _PlayerSpeed;
+		}
+		else{
+			velocity.x = Mathf.MoveToward(velocity.x, 0, _PlayerSpeed);
+			velocity.z = Mathf.MoveToward(velocity.z, 0, _PlayerSpeed);
+		}
+		
+		velocity = MoveAndSlide(velocity, Vector3.Up);
+	}
+	
+	
+	
+}
